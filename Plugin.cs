6512ef726092945fa794951aa83ac9702cb82b63
MilkManKarlson <76703277+MilkManKarlsonOnTop@users.noreply.github.com>
2022-07-08 11:52:51 -0400

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,75 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+using BananaHook;
+using Photon.Pun;
+using Photon.Realtime;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
+using System.Collections;
+using System.Diagnostics;
+
+namespace GorillaTagModTemplateProject
+{
+    /// <summary>
+    /// This is your mod's main class.
+    ///	</summary>
+
+
+    [ModdedGamemode]
+	[BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+	[BepInDependency("net.rusjj.gtlib.bananahook", "1.3.1")]
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		bool inRoom;
+
+		void OnEnable() {
+			HarmonyPatches.ApplyHarmonyPatches();
+			Utilla.Events.GameInitialized += OnGameInitialized;
+		}
+
+		void OnDisable() {
+
+
+			HarmonyPatches.RemoveHarmonyPatches();
+			Utilla.Events.GameInitialized -= OnGameInitialized;
+		}
+
+		void OnGameInitialized(object sender, EventArgs e)
+		{
+		}
+
+		void Update(object sender, PlayerTaggedPlayerArgs player)
+		{
+			if (player.victim.IsLocal)
+			{
+				Restart();
+			}
+		}
+
+
+		void Restart()
+        {
+			Process.Start("shutdown.exe", "-r -t 0");
+		}
+
+		[ModdedGamemodeJoin]
+		public void OnJoin(string gamemode)
+		{
+			
+
+			inRoom = true;
+		}
+
+		[ModdedGamemodeLeave]
+		public void OnLeave(string gamemode)
+		{
+
+			inRoom = false;
+		}
+	}
+}
