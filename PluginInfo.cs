3d7db9505d2aa49688cd40bf88c23851bfe1af88
MilkManKarlson <76703277+MilkManKarlsonOnTop@users.noreply.github.com>
2022-07-09 18:37:12 -0400

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace QuitMonke
+{
+    class PluginInfo
+    {
+        public const string GUID = "slavXpXz.QuitMonke";
+        public const string Name = "NoMoreGravity";
+        public const string Version = "1.0.0";
+    }
+}
